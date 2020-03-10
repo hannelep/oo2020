339f2cc96514b7161449f8ce37c04b9065be4dfd
Hannele <55129216+hannelep@users.noreply.github.com>
2020-03-10 10:54:20 +0200

    viariant2

---
+++ w05_kontrolltoo1/kt/Division.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.File;
+//import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+//import sun.net.www.content.audio.basic;
+
+/**
+ * Division
+ */
+public class Division {
+
+    public static void main(String[] args) throws IOException {
+        //loen failist andmed
+        File file = new File ("arvud.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        //loen failist rea, kuna tean et failis üks rida siis loen ühe rea
+        String line = br.readLine();
+        //failist loetud rea poolitan kohast, kus on "," ning arvud panen arraysse
+        String[] lineData = line.split(",");
+
+        br.close(); //sulgen 
+
+        //Stringid muudan arvudeks
+        int a = Integer.parseInt(lineData[0]);
+        int b = Integer.parseInt(lineData[1]);
+
+        
+        Division d = new Division();
+        double answer = d.divide(a, b);
+        if(answer == 1234567890) {
+            System.out.println("Nulliga ei saa jagada");
+        } else {
+            int length = String.valueOf(answer).length();
+            System.out.println(a + " * " + b + " = " + answer);
+            for( int i = 0; i == length; i++){
+                //int value =(int)answer;
+                String numberString = Double.toString(answer);
+                char letterChar = numberString.charAt(i);
+                int theDigit = Integer.parseInt("" + letterChar);
+                Multiplication m = new Multiplication();
+                int answer2 = m.multiply(b, theDigit);
+                
+                System.out.println(answer2); 
+            }
+        }
+
+    }
+
+    public double divide(int a, int b) {
+        double divide;
+        if(b == 0) {
+            //kui nulliga ei jagu, tagastab selle arvu, hetkel teisiti ei oska
+            divide = 1234567890;
+        } else {
+            divide =(double) a/b;    
+        }
+        return divide;
+
+    }
+
+    
+
+
+
+
+    
+
+
+}
---
+++ w05_kontrolltoo1/kt/Multiplication.java
@@ -0,0 +1,13 @@
+/**
+ * Multiplication
+ */
+public class Multiplication {
+
+    public int multiply(int b, int theDigit ){
+        int multiply;
+
+        multiply = b * theDigit;
+        return multiply;
+        
+    }
+}
