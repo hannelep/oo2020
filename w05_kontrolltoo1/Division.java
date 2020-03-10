f3f1f6270a2375e31901fbe5c9a90fb4a85a0bdc
Hannele <55129216+hannelep@users.noreply.github.com>
2020-03-10 10:41:33 +0200

    kt

---
+++ w05_kontrolltoo1/Division.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.File;
+//import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+//import sun.net.www.content.audio.basic;
+
+/**
+ * Division
+ */
+public class Division {
+
+    public static void main(String[] args) throws IOException {
+        //loen failist andmed
+        File file = new File ("arvud.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        //loen failist rea, kuna tean et failis üks rida siis loen ühe rea
+        String line = br.readLine();
+        //failist loetud rea poolitan kohast, kus on "," ning arvud panen arraysse
+        String[] lineData = line.split(",");
+
+        br.close(); //sulgen 
+
+        //Stringid muudan arvudeks
+        int a = Integer.parseInt(lineData[0]);
+        int b = Integer.parseInt(lineData[1]);
+
+        
+        Division d = new Division();
+        int answer = d.divide(a, b);
+        if(answer == 1234567890) {
+            System.out.println("Nulliga ei saa jagada");
+        } else {
+            int length = String.valueOf(answer).length();
+            System.out.println(a + " * " + b + " = " + answer);
+            for( int i = 0; i < length; i++){
+                String numberString = Integer.toString(answer);
+                char letterChar = numberString.charAt(i);
+                int theDigit = Integer.parseInt("" + letterChar);
+                Multiplication m = new Multiplication();
+                int vastus = m.multiply(b, theDigit);
+                
+                System.out.println(vastus); 
+            }
+        }
+
+    }
+
+    public int divide(int a, int b) {
+        int divide;
+        if(b == 0) {
+            //kui nulliga ei jagu, tagastab selle arvu, hetkel teisiti ei oska
+            divide = 1234567890;
+        } else {
+            divide = a/b;    
+        }
+        return divide;
+
+    }
+
+    
+
+
+
+
+    
+
+
+}
